f164b02a6da43b56ff0a73774eee98899e46cc04
2rnu <gabrielachler@wp.pl>
2023-10-11 15:04:30 +0200

    dx

---
+++ 11_10_2023/pogoda.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Prognoza strony Wrocław </title>
+    <link rel="stylesheet" href="styl2.css">
+</head>
+<body>
+    <header>
+        <div class="left">
+            <img src="logo.png" alt="meteo">
+        </div>
+        <div class="mid">
+            <h1>Prognoza dla Wrocławia</h1>
+        </div>
+        <div class="right">
+            <p>maj, 2019 r.</p>
+        </div>
+    </header>
+    <section>
+        <div class="content">
+            <table>
+                <tr>
+                    <td>
+                        DATA
+                    </td>
+                    <td>
+                        TEMPERATURA W NOCY
+                    </td>
+                    <td>
+                        TEMPERATURA W DZIEŃ
+                    </td>
+                    <td>
+                        OPADY [mm/h]
+                    </td>
+                    <td>
+                        CIŚNIENIE [hPa]
+                    </td>
+                </tr>
+            </table>
+        </div>
+    </section>
+    <section class="foot">
+        <div class="foot-left">
+            <img src="obraz.jpg" alt="Polska, Wrocław">
+        </div>
+        <div class="foot-right">
+            <a href="kwerendy.txt" target="_blank">Pobierz kwerendy</a>
+        </div>
+    </section>
+    <footer>
+    <p>Stronę wykonał: Maksymilian Wesołowski</p>
+    </footer>
+</body>
+</html>
