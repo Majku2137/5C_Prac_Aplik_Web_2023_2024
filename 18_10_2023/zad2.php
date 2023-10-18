92b5f6fc3e5e3b259d0c9a11782865d0b57f36a5
Majku2137 <19c.maksymilian.wesolowski@ezse.pl>
2023-10-18 14:09:17 +0200

    x

--- 18_10_2023/zad2.php
+++ 18_10_2023/zad2.php
@@ -3,16 +3,16 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Zadanie 1</title>
+    <title>Zadanie 2</title>
     <?php 
    
     ?>
 </head>
 <body>
     <form action="" method="post">
-    ID:<input type="text" name="ID" id="ID">
-        Nazwa:<input type="text" name="Nazwa" id="Nazwa">
-        <input type="submit" value="Sprawdź">
+    ID:<input type="text" name="ID" id="ID" requested>
+        Nazwa:<input type="text" name="Nazwa" id="Nazwa" requested>
+        <input type="submit" value="Sprawdź" >
         
         <?php
                 $host = "localhost";
