a4508b616fe3a723adc826f056dc9a60978f684b
Majku2137 <19c.maksymilian.wesolowski@ezse.pl>
2023-11-08 14:06:17 +0100

    08

---
+++ 08_11_2023_Warunkowe/sprawdz_umiejetnosci/zad1.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $a = 4;
+        $b = 5;
+
+        if ($a > $b) {
+            echo"$a";
+        }elseif ($a < $b) {
+            echo "$b";
+        }
+    ?>
+</body>
+</html>
